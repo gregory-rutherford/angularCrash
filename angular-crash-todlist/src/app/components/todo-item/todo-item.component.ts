f954e3b6a0e7ebc443946907abde87fb39ed10eb
Gregory Rutherford <46570985+gregory-rutherford@users.noreply.github.com>
2019-11-17 19:18:17 -0600

    progress, more

--- angular-crash-todlist/src/app/components/todo-item/todo-item.component.ts
+++ angular-crash-todlist/src/app/components/todo-item/todo-item.component.ts
@@ -2,24 +2,30 @@ import { Component, OnInit, Input } from '@angular/core';
 import {ToDo} from "src/app/Models/ToDo";
 
 @Component({
-  selector: 'app-todo-item',
-  templateUrl: './todo-item.component.html',
-  styleUrls: ['./todo-item.component.css']
+  selector: "app-todo-item",
+  templateUrl: "./todo-item.component.html",
+  styleUrls: ["./todo-item.component.css"]
 })
 export class TodoItemComponent implements OnInit {
   @Input() todo: ToDo;
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   //set dynamic classses
   setClasses() {
     let classes = {
       todo: true,
-      'is-complete': this.todo.completed
-    }
+      "is-complete": this.todo.completed
+    };
     return classes;
   }
 
+  onToggle(todo) {
+    todo.completed = !todo.completed;
+  }
+
+  onDelete(todo) {
+    console.log("delete");
+  }
 }
